8a563296dd77ed8af74243f1929f0a505e92283c
rodrigo-sys <chavezrodrigo001@gmail.com>
2024-01-22 19:08:26 -0300

    change format to fcc suport

--- myApp.js
+++ myApp.js
@@ -5,9 +5,19 @@ let app = express();
 app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') })
 app.use('/public', express.static(__dirname + '/public'));
 
+console.log(process.env.MESSAGE_STYLE)
+
 app.get('/json', (req, res) => {
+  /*
   message = "Hello json"
   message = process.env.MESSAGE_STYLE == 'uppercase' ? message.toUpperCase() : message;
+  res.json({message})
+  */
+
+  message = "Hello json"
+  if(process.env.MESSAGE_STYLE == 'uppercase'){ 
+    message = message.toUpperCase() 
+  }
 
   res.json({message})
 });
