23cec9a4a637291cfda099d92929e82931941a0e
rodrigo-sys <chavezrodrigo001@gmail.com>
2024-01-22 18:07:59 -0300

    add json route

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let app = express();
-console.log('Hello World')
+// console.log('Hello World')
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') })
 app.use('/public', express.static(__dirname + '/public'));
-// app.use(express.static(__dirname + "/public"));
 
+app.get('/json', (req, res) => res.json({"message": "Hello json"}));
 
 
 
