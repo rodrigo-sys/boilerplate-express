b07de55e13dda75a428a04e594bef0194b832bd1
rodrigo-sys <chavezrodrigo001@gmail.com>
2024-01-22 18:55:02 -0300

    conditional case on json route

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
-// console.log('Hello World')
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') })
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', (req, res) => res.json({"message": "Hello json"}));
+app.get('/json', (req, res) => {
+  message = "Hello json"
+  message = process.env.MESSAGE_STYLE == 'uppercase' ? message.toUpperCase() : message;
+
+  res.json({message})
+});
 
 
 
@@ -41,4 +46,4 @@ app.get('/json', (req, res) => res.json({"message": "Hello json"}));
 
 
 
- module.exports = app;
+module.exports = app;
