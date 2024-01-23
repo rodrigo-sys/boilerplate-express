f62010350c96376017c25b1e2436d96c1fe27e85
rodrigo-sys <chavezrodrigo001@gmail.com>
2024-01-23 12:56:49 -0300

    made it root-level logger

--- myApp.js
+++ myApp.js
@@ -2,58 +2,22 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') })
-app.use('/public', express.static(__dirname + '/public'));
+app.use((req, res, next) => { 
+  console.log(`${req.method} ${req.path} - ${req.ip}`) 
+  next()
+});
 
-console.log(process.env.MESSAGE_STYLE)
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') })
 
 app.get('/json', (req, res) => {
-  /*
   message = "Hello json"
-  message = process.env.MESSAGE_STYLE == 'uppercase' ? message.toUpperCase() : message;
-  res.json({message})
-  */
 
-  message = "Hello json"
   if(process.env.MESSAGE_STYLE == 'uppercase'){ 
     message = message.toUpperCase() 
   }
 
   res.json({message})
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
